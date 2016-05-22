00313031f1fe07f6d6733a122acbf809dd866fbb
xxxspy <675495787@QQ.COM>
2016-05-22 10:17:25 +0800

    add changyan pinglun

--- nbviewer/providers/local/handlers.py
+++ nbviewer/providers/local/handlers.py
@@ -46,13 +46,44 @@ class LocalFileHandler(RenderingHandler):
         if not os.path.exists(abspath):
             raise web.HTTPError(404)
         if not os.path.isfile(abspath):
-            pass
+            base_url='/localfile'
+            contents=os.listdir(abspath)
+            dirs=[]
+            others=[]
+            ipynbs=[]
+            entries=[]
+            for c in contents:
+                e={}
+                e['name']=c 
+                e['url']=os.path.join(base_url,path,c)
+                subpath=os.path.join(abspath,c)
+                if os.path.isdir(subpath):
+                    e['class']='fa-folder-open'
+                    dirs.append(e)
+                elif os.path.isfile(subpath):
+                    if c.endswith('.ipynb'):
+                        e['class']='fa-book'
+                        ipynbs.append(e)
+                    else:
+                        e['url']='javascript:alert("not allowed type!");'
+                        e['class']='fa-folder-close'
+                        others.append(e)
+            entries.extend(dirs)
+            entries.extend(ipynbs)
+            entries.extend(others)
+            breadcrumbs=self.breadcrumbs(path,base_url)
+            html=self.render_template('treelist.html',
+                entries=entries,breadcrumbs=breadcrumbs,tree_type='localfile',
+                tree_label='localfiels')
+            yield self.cache_and_finish(html)
         else:
             with io.open(abspath, encoding='utf-8') as f:
                 nbdata = f.read()
-
+            provider_url=os.path.join(base_url,path)
             yield self.finish_notebook(nbdata, download_url=path,
+                                        provider_url=provider_url,
                                        msg="file from localfile: %s" % path,
                                        public=False,
                                        format=self.format,
                                        request=self.request)
+
